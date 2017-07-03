1ae6fec0bba5bbf4ccf2bb79e96eea239190afd5
gclement <gclement@LAPTOP777362.local>
2017-07-03 08:55:05 +0200

    Fixes #8 : special characters in username

--- specs/mocktest.js
+++ specs/mocktest.js
@@ -3,8 +3,12 @@
 
 var ow = require('../index.js');
 
-ow.search('Zeya-2303')
+ow.search('SĔNPAI-1698')
     .then((data) => console.dir(data, {depth : 3, colors : true}) );
 
 ow.getAll('pc', 'eu', 'Zeya-2303')
+    .then((data) => console.dir(data, {depth : 2, colors : true}) );
+
+//// Special characters
+ow.getAll('pc', 'us', 'SĔNPAI-1698')
     .then((data) => console.dir(data, {depth : 2, colors : true}) );
